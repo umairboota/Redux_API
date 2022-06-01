3d283ec7691a479279b2d074d88f8edd375a7303
umairboota <mumairboota007@gmail.com>
2022-06-01 10:58:57 +0500

    completing

--- src/Compoenets/Header.js
+++ src/Compoenets/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="/">
           Shop
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
--- src/Compoenets/ProductsDetail.js
+++ src/Compoenets/ProductsDetail.js
@@ -30,38 +30,42 @@ const ProductsDetail = () => {
   }, [productId]);
   return (
     <>
-    <div className="container mt-4">
-      {product? (
-        <div className="row">
-          <div className="col-md-6">
-            <img src={image} style={{ height: "40rem" }} alt="...Loading" />
-          </div>
-          <div className="col-4 col-md-4">
-            <h3 className="text-secondary mt-4">
-              {id}.{title}
-            </h3>
-            <div className="mt-4">
-              <h6 className="text-success">Category: {category}</h6>
+      <div className="container mt-4">
+        {product ? (
+          <div className="row">
+            <div className="col-md-6">
+              <img src={image} style={{ height: "40rem" }} alt="...Loading" />
             </div>
-            <div className="mt-4">
-              <h5>Description*</h5>
-              <p>{description}</p>
-              <h6 className="text-secondary">Price: ${price}</h6>
-            </div>
-            <div className="mt-4">
-              <div className="d-grid gap-2">
-                <button className="btn btn-primary" type="button">
-                  Add to cart
-                </button>
-                <button className="btn btn-success" type="button">
-                  Buy Now
-                </button>
+            <div className="col-4 col-md-4">
+              <h3 className="text-secondary mt-4">
+                {id}.{title}
+              </h3>
+              <div className="mt-4">
+                <h6 className="text-success">Category: {category}</h6>
+              </div>
+              <div className="mt-4">
+                <h5>Description*</h5>
+                <p>{description}</p>
+                <h6 className="text-secondary">Price: ${price}</h6>
+
+                {/* <h6>Ratings: {rating}</h6> */}
+              </div>
+              <div className="mt-4">
+                <div className="d-grid gap-2">
+                  <button className="btn btn-primary" type="button">
+                    Add to cart
+                  </button>
+                  <button className="btn btn-success" type="button">
+                    Buy Now
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ):(<LoaderComponenet/>)}
-    </div>
+        ) : (
+          <LoaderComponenet />
+        )}
+      </div>
     </>
   );
 };
